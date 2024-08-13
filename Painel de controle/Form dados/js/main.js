8cbeba45f1fe3ca52b1d988830f61f1e84869d56
Julio-W <jwfp@aluno.ifnmg.edu.br>
2024-08-13 11:17:08 -0300

    att2

---
+++ Painel de controle/Form dados/js/main.js
@@ -0,0 +1,190 @@
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+// Validate CEP Input
+cepInput.addEventListener("keypress", (e) => {
+  const onlyNumbers = /[0-9]|\./;
+  const key = String.fromCharCode(e.keyCode);
+
+  console.log(key);
+
+  console.log(onlyNumbers.test(key));
+
+  // allow only numbers
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
+});
+
+// Evento to get address
+cepInput.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value;
+
+  //   Check if we have a CEP
+  if (inputValue.length === 8) {
+    getAddress(inputValue);
+  }
+});
+
+// Get address from API
+const getAddress = async (cep) => {
+  toggleLoader();
+
+  cepInput.blur();
+
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const response = await fetch(apiUrl);
+
+  const data = await response.json();
+
+  console.log(data);
+  console.log(formInputs);
+  console.log(data.erro);
+
+  // Show error and reset form
+  if (data.erro === "true") {
+    if (!addressInput.hasAttribute("disabled")) {
+      toggleDisabled();
+    }
+
+    addressForm.reset();
+    toggleLoader();
+    toggleMessage("CEP Inválido, tente novamente.");
+    return;
+  }
+
+  // Activate disabled attribute if form is empty
+  if (addressInput.value === "") {
+    toggleDisabled();
+  }
+
+  addressInput.value = data.logradouro;
+  cityInput.value = data.localidade;
+  neighborhoodInput.value = data.bairro;
+  regionInput.value = data.uf;
+
+  toggleLoader();
+};
+
+// Add or remove disable attribute
+const toggleDisabled = () => {
+  if (regionInput.hasAttribute("disabled")) {
+    formInputs.forEach((input) => {
+      input.removeAttribute("disabled");
+    });
+  } else {
+    formInputs.forEach((input) => {
+      input.setAttribute("disabled", "disabled");
+    });
+  }
+};
+
+// Show or hide loader
+const toggleLoader = () => {
+  const fadeElement = document.querySelector("#fade");
+  const loaderElement = document.querySelector("#loader");
+
+  fadeElement.classList.toggle("hide");
+  loaderElement.classList.toggle("hide");
+};
+
+// Show or hide message
+const toggleMessage = (msg) => {
+  const fadeElement = document.querySelector("#fade");
+  const messageElement = document.querySelector("#message");
+
+  const messageTextElement = document.querySelector("#message p");
+
+  messageTextElement.innerText = msg;
+
+  fadeElement.classList.toggle("hide");
+  messageElement.classList.toggle("hide");
+};
+
+// Close message modal
+closeButton.addEventListener("click", () => toggleMessage());
+
+// Save address
+addressForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  toggleLoader();
+
+  setTimeout(() => {
+    toggleLoader();
+
+    toggleMessage("Endereço salvo com sucesso!");
+
+    addressForm.reset();
+
+    toggleDisabled();
+  }, 1000);
+});
+
+
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
