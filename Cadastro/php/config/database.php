663a0067926930282ff4b1ed5f4c443712af2405
ItaloFeSo <ji1173774@gmail.com>
2024-09-14 20:42:36 -0300

    teste

--- Cadastro/php/config/database.php
+++ Cadastro/php/config/database.php
@@ -8,7 +8,7 @@ $dbname = "smartparker";
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
+// Verifica a conexão teste
 if ($conn->connect_error)   
  {
     die("Connection failed: " . $conn->connect_error);   
